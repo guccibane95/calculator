a4b1069c0a322105e2b7be89e2af4b2051bc150f
Branko Prica <branko.prica@gmail.com>
2023-02-27 21:00:16 +0000

    added datakeys for most. not sure how to add keydown eventlistener

---
+++ java.js
@@ -0,0 +1,44 @@
+/*Math functions */
+function add(a,b){ 
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+
+
+let display = 5
+document.getElementById("screen").innerHTML=display
+
+
+
+function operate(operator, a, b){
+    if (operator==="add"){
+        return add(a,b)
+    } else if (operator==="subtract"){
+        return subtract(a,b)
+    } else if (operator==="multiply"){
+        return multiply(a,b)
+    } else if (operator==="divide"){
+        return divide(a,b)
+    }
+}
+
+
+/* working on keystroke of button to show on display
+function displayKey(e){
+    let num = document.querySelector(`num[data-key]="${e.keyCode}"`)
+    console.log(num)
+}
+window.addEventListener('keydown', function (e) {
+    const num=document.querySelector(`button[data*="${e.keyCode}"]`)
+    console.log(num)
+
+})
+*/
